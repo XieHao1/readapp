0eb3a24ee62ccb62ae46195bc3a4da0afe7647fa
谢豪 <1693062665@qq.com>
2022-04-26 22:40:40 +0800

    增加个人文章排序，修改banner图

--- src/main/java/com/xh/readapp/Controller/ImageController.java
+++ src/main/java/com/xh/readapp/Controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.xh.readapp.Controller;
 
+import com.xh.readapp.common.Cache;
 import com.xh.readapp.util.ResultJson;
 import com.xh.readapp.vo.image.ImageVo;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +15,17 @@ import java.util.List;
 public class ImageController {
 
     @GetMapping("/banner")
+    @Cache(expire = 24*60*60*1000,name = "图片缓存")
     public ResultJson banner(){
         List<ImageVo> list = new ArrayList<>();
         list.add(new ImageVo(1,"http://codexh.xyz/1/banner-01.jpg"));
-        list.add(new ImageVo(2,"http://codexh.xyz/1/banner-02.jpg"));
+        list.add(new ImageVo(2,"http://codexh.xyz/1/20220426215845.png"));
         list.add(new ImageVo(3,"http://codexh.xyz/1/banner-03.jpg"));
         return ResultJson.success(list);
     }
 
     @GetMapping("/picture")
+    @Cache(expire = 24*60*60*1000,name = "图片缓存")
     public ResultJson picture(){
         List<ImageVo> list = new ArrayList<>();
         list.add(new ImageVo(1,"http://codexh.xyz/1/20220426111358.png"));
